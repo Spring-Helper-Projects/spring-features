50cc07911cd08441973bd792e98981fa7a7de6b9
forbodingangel <forbodingangel@37b2f9d1-402a-082a-f3f4-8f1a2a763f67>
2011-09-04 01:39:06 +0000

    Committing latest mapconv plus all tools (missing grout, if anyone has it, please commit it)

---
+++ mapconv + tools/compile.bat
@@ -0,0 +1,3 @@
+mapconv --help
+MapConv -i -c 0 -x 101 -n 100 -o MyNewMap.smf -t terrain.png -a height.png -m metal.png -f feature.png -z "nvdxt.exe -dxt1a -nmips 4 -Sinc -quality_highest -file"
+pause
