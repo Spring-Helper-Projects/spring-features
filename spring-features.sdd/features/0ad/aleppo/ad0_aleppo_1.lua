47f4f233805c5f0dc424ef7e72ad26a242a80a5c
forbodingangel <forbodingangel@37b2f9d1-402a-082a-f3f4-8f1a2a763f67>
2012-04-19 22:57:23 +0000

    Adding 0AD Aleppo Pine features

---
+++ spring-features.sdd/features/0ad/aleppo/ad0_aleppo_1.lua
@@ -0,0 +1,21 @@
+local objectname= "ad0_aleppo_1" 
+local featureDef	=	{
+	name			= "ad0_aleppo_1",
+	world				="All Worlds",
+	description				="Aleppo pine",
+	category				="Vegetation",
+	object				="features/0ad/aleppo/ad0_aleppo_1.s3o",
+	footprintx				=0,
+	footprintz				=0,
+	height				=30,
+	blocking				=true,
+	upright				=true,
+	hitdensity				=0,
+	energy				=1,
+	metal				=0,
+	damage				=30,
+	flammable				=true,
+	reclaimable				=true,
+	autoreclaimable				=true,
+} 
+return lowerkeys({[objectname] = featureDef}) 
